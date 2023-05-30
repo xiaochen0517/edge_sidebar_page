e3830dc8777840d8af85d68528eb28a38d1f2113
陌尘吖 <2827075398@qq.com>
2023-05-30 11:42:57 +0800

    完成 v0.0.1

---
+++ main.js
@@ -0,0 +1,110 @@
+import {
+    createApp
+} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/+esm'
+import mdui from 'https://cdn.jsdelivr.net/npm/mdui@1.0.2/+esm'
+
+createApp({
+    data() {
+        return {
+            title: 'Edge Sidebar Site',
+            siteList: [],
+            addSitePageDialog: null,
+            dialogData: {
+                form: {
+                    icon: '',
+                    url: '',
+                    name: ''
+                },
+                invalid: {
+                    icon: '',
+                    url: '',
+                    name: ''
+                },
+            },
+            defaultDialogData: {
+                form: {
+                    icon: '',
+                    url: '',
+                    name: ''
+                },
+                invalid: {
+                    icon: '',
+                    url: '',
+                    name: ''
+                },
+            }
+        }
+    },
+    mounted() {
+        this.initDialog()
+        this.loadSiteList()
+    },
+    methods: {
+        initDialog() {
+            this.addSitePageDialog = new mdui.Dialog(this.$refs.addSitePageDialogRefs)
+        },
+        loadSiteList() {
+            this.siteList.splice(0, this.siteList.length)
+            const siteListJson = Cookies.get('siteList')
+            console.log('siteList', siteListJson);
+            if (siteListJson) {
+                const siteList = JSON.parse(siteListJson)
+                for (const siteInfo of siteList) {
+                    this.siteList.push(siteInfo)
+                }
+            }
+        },
+        goToNewSite(siteInfo) {
+            window.open(siteInfo.url)
+        },
+        openAddSitePageDialog() {
+            // 清除对话框内容
+            this.cleanDialogData()
+            this.addSitePageDialog.open()
+        },
+        confirmAddSitePage() {
+            // 验证添加的内容
+            if (!this.validateDialogData()) return
+            // 将site添加到siteList
+            this.siteList.push(this.dialogData.form)
+            // 将siteList保存到cookie
+            Cookies.set('siteList', JSON.stringify(this.siteList))
+            // 关闭对话框
+            this.addSitePageDialog.close()
+        },
+        validateDialogData() {
+            let valid = true
+            const form = this.dialogData.form
+            // icon不可为空且需要是一个url，使用正则表达式验证
+            if (!form.icon || !form.icon.match(/^(http|https):\/\/.*\.(png|jpg|jpeg|gif|svg)$/)) {
+                this.dialogData.invalid.icon = '请输入正确的icon地址'
+                valid = false
+            } else {
+                this.dialogData.invalid.icon = ''
+            }
+            // url不可为空且需要是一个url，使用正则表达式验证
+            if (!form.url || !form.url.match(/^(http|https):\/\/.*$/)) {
+                this.dialogData.invalid.url = '请输入正确的url地址'
+                valid = false
+            } else {
+                this.dialogData.invalid.url = ''
+            }
+            // name不可为空
+            if (!form.name) {
+                this.dialogData.invalid.name = '请输入名称'
+                valid = false
+            } else {
+                this.dialogData.invalid.name = ''
+            }
+            return valid
+        },
+        cleanDialogData() {
+            this.dialogData = JSON.parse(JSON.stringify(this.defaultDialogData))
+        },
+        deleteCurrentSite(index) {
+            this.siteList.splice(index, 1)
+            Cookies.set('siteList', JSON.stringify(this.siteList))
+        }
+    },
+}).mount('#app')
